53d1da48917e3f043c6462fc8f218bd9062d9a3a
Anthony Cooper <anthonycooper@gmail.com>
2019-07-29 13:14:26 -0500

    Game won feature completed. Minor bug fixes and function decomposition.

--- js/main.js
+++ js/main.js
@@ -98,7 +98,7 @@ function placeBid(playerControl) {
     var player = findPlayer(name);
 
     // Wallet in player control for updating.
-    var walletAmountElement = playerControl.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling;    
+    var walletAmountElement = playerControl.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling;
     var w = player.wallet;
     var newWalletAmount = w - 1;
 
@@ -130,22 +130,9 @@ function placeBid(playerControl) {
     else {
         player.bid = player.bid + 1;
 
-        // First row is currently the queue labels, so will be thrown away.
-        var playerSearchElement = q.firstElementChild;
-        var playerSortElement;
-
-        // console.log("bid update in queue");
-        // Get the bid value and update. Uneccesarily long - search is known to succeed, and the player names will be unique.
-        for (var i = 0; i < q.childElementCount - 1; i++) {
-            playerSearchElement = playerSearchElement.nextElementSibling;
-            // console.log("count:", i);
-            // console.log("Queue content:", playerQueueElement);
-            if (playerSearchElement.firstElementChild.textContent == name){
-                playerSearchElement.firstElementChild.nextElementSibling.textContent = player.bid;
-                playerSortElement = playerSearchElement;
-                }
-            }
-        
+        var playerSortElement = getPlayerRowFromQueue(q, name);
+//        console.log("getPlayerRowFromQueue result:", playerSortElement);
+        playerSortElement.firstElementChild.nextElementSibling.textContent = player.bid;
         // Finally, sort queue. Assume the top player is not bidding.
         // Get all the relevant additional data.
         var previousPlayer = playerSortElement.previousElementSibling;
@@ -192,33 +179,59 @@ function declareWinner(form) {
 
         var bountyElement = document.getElementById("bounty_amount_element");
         let bounty = parseInt(bountyElement.textContent);
-        
+        var winnerControlPanel;
+        var bid, newWallet, newBounty, newProfit;
+        var queue = document.getElementById('queue');
+        var playerWonQueueElement, playerLostQueueElement;
         if (playersRadioArray[0].checked) {
             console.log("Champ branch selected");
-            let bid = challengerObject.bid;
-            let newWallet = champObject.wallet + bid * (1/4);
-            let newBounty = bounty + bid * (1/4);
-            let newProfit = profit + bid * (1/2);
+            bid = challengerObject.bid;
+            newWallet = champObject.wallet + bid * (1/4);
+            newBounty = bounty + bid * (1/4);
+            newProfit = profit + bid * (1/2);
 
+            challengerObject.bid = 0;
             champObject.wallet = newWallet;
             bountyElement.textContent = newBounty;
             profit = newProfit;
+            winnerControlPanel = document.getElementById(champObject.name.replace(/ /g, '_') + '_control_panel');
+            playerWonQueueElement = document.getElementById(champObject.name.replace(/ /g, '_') + '_queue');
+            playerLostQueueElement = getPlayerRowFromQueue(queue, challengerObject.name);
             }
         else {
             console.log("Challenger selected");
-            let bid = champObject.bid;
-            let newWallet = challengerObject.wallet + bounty + bid * (1/4);
-            let newBounty = bid * (1/4);
-            let newProfit = profit + bid * (1/2);
+            bid = champObject.bid;
+            newWallet = challengerObject.wallet + bounty + bid * (1/4);
+            newBounty = bid * (1/4);
+            newProfit = profit + bid * (1/2);
 
+            champObject.bid = 0;
             challengerObject.wallet = newWallet;
             bountyElement.textContent = newBounty;
             profit = newProfit;
+            winnerControlPanel = document.getElementById(challengerObject.name.replace(/ /g, '_') + '_control_panel');
+            playerWonQueueElement = document.getElementById(challengerObject.name.replace(/ /g, '_') + 'queue');
+            playerLostQueueElement = getPlayerRowFromQueue(queue, champObject.name);
             }
+        console.log(winnerControlPanel);
+        var walletElement = winnerControlPanel.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling;
+        walletElement.textContent = newWallet;
+        console.log("Player queue element:", playerLostQueueElement);
+        queue.removeChild(playerLostQueueElement);
         console.log("Final player objects:", champObject, challengerObject);
 
-//        form.reset();
+        let champRowName = queue.firstElementChild.nextElementSibling.firstElementChild.textContent;
+        let challengerRowName = queue.firstElementChild.nextElementSibling.nextElementSibling.firstElementChild.textContent;
+        console.log(champRowName, challengerRowName);
+        var champInput = document.getElementById("champ_radio_input");
+        var challengerInput = document.getElementById("challenger_radio_input");
+        champInput.nextElementSibling.textContent = champRowName;
+        challengerInput.nextElementSibling.textContent = challengerRowName;
+        champInput.value = champRowName;
+        challengerInput.value = challengerRowName;
+        form.reset();
         }
+    logPlayersArray();
     }
 function findPlayer(name) {
     var player = playersArray.find(function(o) {
@@ -232,4 +245,17 @@ function logPlayersArray() {
         var p = playersArray[i];
         console.log(p.name, p.wallet, p.bid);
         }
+    }
+// May not need to include queue. Depends on if the dom interface is by reference or by value.
+function getPlayerRowFromQueue(queue, name) {
+    // First element is the title row, and needs to be skipped.
+    var playerSearchElement = queue.firstElementChild;
+    var playerSortElement;
+    for (var i = 0; i < queue.childElementCount - 1; i++) {
+        playerSearchElement = playerSearchElement.nextElementSibling;
+        if (playerSearchElement.firstElementChild.textContent == name){
+            playerSortElement = playerSearchElement;
+            }
+        }
+    return playerSortElement;
     }
